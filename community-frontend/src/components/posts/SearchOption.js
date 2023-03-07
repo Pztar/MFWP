78c8a7b45e6e874130d4a178157e5708d76a7b98
Pztar <phs559800@gmail.com>
2023-03-07 23:13:14 +0900

    검색기능 프론트 수정

--- community-frontend/src/components/posts/SearchOption.js
+++ community-frontend/src/components/posts/SearchOption.js
@@ -7,38 +7,39 @@ const Spacer = styled.div`
   height: 1rem;
 `;
 
-const PaginationBlock = styled.div`
+const SearchOptionBlock = styled.div`
   width: 320px;
   margin: 0 auto 0;
   display: flex;
   justify-content: space-between;
 `;
 
-const buildLink = ({ userId, title, content, userNick, hashtag, page }) => {
+const buildLink = ({ userId, hashtag, title, content, userNick, page }) => {
   const query = qs.stringify({ title, content, userNick, hashtag, page });
   return userId ? `/posts/${userId}?${query}` : `/posts/?${query}`;
 };
 
-const SearchOption = ({ userId }) => {
+const SearchOption = ({ userId, hashtag }) => {
   const [selected, setSelected] = useState("title");
   const [searchWord, setSearchWord] = useState("");
   return (
     <>
-      <PaginationBlock>
+      <SearchOptionBlock>
         <select onChange={(e) => setSelected(e.target.value)} value={selected}>
           <option value="title">제목</option>
           <option value="content">내용</option>
           <option value="userNick">작성자</option>
-          <option value="hashtag">해쉬태그</option>
         </select>
         <input
           onChange={(e) => setSearchWord(e.target.value)}
           value={searchWord}
         />
-        <Button to={buildLink({ userId, [selected]: searchWord, page: 1 })}>
+        <Button
+          to={buildLink({ userId, hashtag, [selected]: searchWord, page: 1 })}
+        >
           검색
         </Button>
-      </PaginationBlock>
+      </SearchOptionBlock>
       <Spacer />
     </>
   );
--- community-frontend/src/containers/posts/SearchOptionContainer.js
+++ community-frontend/src/containers/posts/SearchOptionContainer.js
@@ -1,10 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import qs from "qs";
 import SearchOption from "../../components/posts/SearchOption";
 
 const SearchOptionContainer = () => {
   const { userId } = useParams();
+  const { search } = useLocation();
+  const { hashtag } = qs.parse(search, {
+    ignoreQueryPrefix: true,
+  });
 
-  return <SearchOption userId={userId} />;
+  return <SearchOption hashtag={hashtag} userId={userId} />;
 };
 
 export default SearchOptionContainer;
